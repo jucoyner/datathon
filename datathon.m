db702dc2e376111b98a3aa1ed2e4593a236a0db9
Julia Coyner <47306935+jucoyner@users.noreply.github.com>
2020-01-25 10:14:13 -0600

    Add files via upload

--- datathon.m
+++ datathon.m
@@ -1,11 +1,21 @@
+% this code plots the training data features in relation to the rate of
+% penetration
+% we found that the formation ID has many outliers around the value 221, so
+% we plotted each feature in relation to both formation ID and rate of
+% penetration
+
 close all;
 
 training = readmatrix('training.csv');
 
-
+% plots the correlation matrix for each of the features with eachother
+% note the large correlation between min depth and max depth
 corrplot(training(:,2:11));
 set(findall(gcf, 'type', 'axes'), 'XTickLabel', [])
 
+
+% the below figures are where we plotted each feature against rate of
+% penetration and formation ID
 
 figure
 scatter3(training(:,9),training(:,2),training(:,4));
@@ -16,11 +26,6 @@ figure
 scatter3(training(:,9),training(:,3),training(:,4));
 xlabel('rate of penetration');
 ylabel('areaid')
-
-figure
-scatter(training(:,9),training(:,4));
-xlabel('rate of penetration');
-ylabel('formationid')
 
 figure
 scatter3(training(:,9),training(:,5),training(:,4));
@@ -52,3 +57,10 @@ scatter3(training(:,9),training(:,11),training(:,4));
 xlabel('rate of penetration');
 ylabel('surface rpm')
 
+
+% this plot shows the large number of outliers at the value 221 for
+% formation ID
+figure
+scatter(training(:,9),training(:,4));
+xlabel('rate of penetration');
+ylabel('formationid')
